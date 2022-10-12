8fc8deacd1e04bca92719346a36811dad6112ebd
AGUSTINA YAEL ANGEL <46645633@ort>
2022-10-12 12:09:09 -0300

    archivos

--- TP09/wwwroot/js/site.js
+++ TP09/wwwroot/js/site.js
@@ -2,3 +2,24 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function CargarDatos()
+{
+    $.getJSON('/TP09json.json', function(data) {
+        console.log(data);
+
+        data.restaurantes.forEach(unrestaunte =>  {
+            $("#bodyTabla").append("<tr>");
+            $("#bodyTabla").append("<td><img src='/imagenes/" + unrestaunte.Logo + "'></td>"); 
+            $("#bodyTabla").append("<td>" + unrestaunte.ID + "</td>"); 
+            $("#bodyTabla").append("<td>" + unrestaunte.Nombre + "</td>"); 
+            $("#bodyTabla").append("<td>" + unrestaunte.Apertura + "</td>"); 
+            $("#bodyTabla").append("<td>" + unrestaunte.Ubicacion + "</td>"); 
+            $("#bodyTabla").append("<td>" + unrestaunte.Duenios + "</td>"); 
+            $("#bodyTabla").append("<tr>");
+        });
+        
+        }
+     );
+}
+
